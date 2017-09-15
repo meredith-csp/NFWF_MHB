f54871a5f18c0a3b8512e5386638c9b1bae76448
Meredith <meredith@csp-inc.org>
2017-09-15 09:46:57 -0700

    Cleaned up code a bit

--- app.R
+++ app.R
@@ -2,9 +2,8 @@
 
 library(shiny)
 library(rsconnect)
-#rsconnect::deployApp('/Users/meredith-csp/repos/NFWF_MHB')
 
-#Define input data and fixed parameters here:
+#Define input data and fixed parameters globally here:
 targ.summ<-read.csv("targets.csv")
 
 ui<-fluidPage(titlePanel("Missouri Headwaters Basin Conservation Prioritization Tool"),
@@ -16,18 +15,12 @@ ui<-fluidPage(titlePanel("Missouri Headwaters Basin Conservation Prioritization 
                   wellPanel(h5("Select conservation targets of interest:"),
                               checkboxGroupInput("targ.sel","",choiceNames=as.list(as.character(targ.summ$name)),choiceValues=as.list(as.character(targ.summ$abbv)))),
                   wellPanel(h5("Set weights for each target:"),
-                              #sliderInput("rh.wt", "Riparian Health",min=0.0,max=10.0,value="1.0",step=1.0,round=FALSE),
-                              #sliderInput("rc.wt", "Riparian Configuration",min=0.0,max=10.0,value="1.0",step=1.0,round=FALSE),
-                              #sliderInput("uv.wt", "Upland Vegetation",min=0.0,max=10.0,value="1.0",step=1.0,round=FALSE),
-                              #sliderInput("whc.wt", "Within Habitat Connectivity",min=0.0,max=10.0,value="1.0",step=1.0,round=FALSE),
-                              #sliderInput("nfr.wt", "Normative Flow Regime",min=0.0,max=10.0,value="1.0",step=1.0,round=FALSE)),
-                              textOutput("wt.sel0"),
                               uiOutput("wt.sel")),
                   wellPanel(h5("Set run parameters:"),
                             radioButtons("rem.rule","Choose removal rule",c("Basic Core-Area Zonation"="bca","Additive Benefit Function"="abf","Target-Based Planning"="tbp","Generalized Benefit Function"="gbf","Random Removal"="rr"),selected="abf"),
                             sliderInput("warp","High Quality <---------------> High Speed",min=50.0,max=2000.0,value=100,step=10.0,round=FALSE,ticks=FALSE,width='80%')
                             )),
-                #Output: eventually mapped priorities, for now show selected targets, params
+                #Output: map, summary plots, etc.
                 mainPanel(
                   wellPanel(#h5("Selected run parameters:"),
                             textOutput("targ.nm"),
